39311b947551e23f41ef8606bef36a77d3af4684
Mehul Singh <mehulsingh2022@vitbhopal.ac.in>
2024-08-22 05:11:21 +0530

    .

--- src/components/admin-panel/sheet-menu.jsx
+++ src/components/admin-panel/sheet-menu.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { MenuIcon, PanelsTopLeft } from "lucide-react";
+import { Globe, Globe2, MenuIcon, PanelsTopLeft, WholeWord } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Menu } from "@/components/admin-panel/menu";
@@ -27,7 +27,7 @@ export function SheetMenu() {
             asChild
           >
             <Link href="/dashboard" className="flex items-center gap-2">
-              <PanelsTopLeft className="w-6 h-6 mr-1" />
+              <Globe2 className="w-6 h-6 mr-1" />
               <SheetTitle className="font-bold text-lg">AirGlobe</SheetTitle>
             </Link>
           </Button>
